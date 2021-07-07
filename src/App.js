0cd97fa345a5ac2d7e335f82788a96b64210690b
Mehulll <mehul.aryan.2222@gmail.com>
2021-07-07 17:38:29 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Topbar from "./components/topbar/Topbar.jsx"
+import Sidebar from "./components/sidebar/Sidebar.jsx"
+import './app.scss'
+import Home from "./pages/home/Home.jsx"
+import UserList from "./pages/userList/UserList.jsx"
+import User from "./pages/user/User.jsx"
+import NewUser from "./pages/newUser/NewUser.jsx"
+import ProductList from "./pages/productList/ProductList.jsx"
+import Product from "./pages/product/Product.jsx"
+import NewProduct from "./pages/newProduct/NewProduct.jsx"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar/>
+      <div className="container">
+        <Sidebar/>
+          <Switch>
+            <Route exact path = "/">
+              <Home/>
+            </Route>
+            <Route path = "/users">
+              <UserList/>
+            </Route>
+            <Route path = "/user/:userId">
+              <User/>
+            </Route>
+            <Route path = "/newUser">
+              <NewUser/>
+            </Route>
+            <Route path = "/products">
+              <ProductList/>
+            </Route>
+            <Route path = "/product/:productsId">
+              <Product/>
+            </Route>
+            <Route path = "/newProduct">
+              <NewProduct/>
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
